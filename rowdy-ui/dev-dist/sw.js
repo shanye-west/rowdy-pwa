56a6b43c4380b01400de8323d83c90f75497137e
shanye-west <shanepeterson32@gmail.com>
2025-12-01 06:48:16 -0800

    fix: update revision number for index.html in service worker

--- rowdy-ui/dev-dist/sw.js
+++ rowdy-ui/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-fa6cb374'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.vsugt0ncrqk"
+    "revision": "0.l3rtno5asp8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
