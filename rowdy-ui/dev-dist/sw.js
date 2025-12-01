ad37b7666563f020764b176e6436661c64472d2a
shanye-west <shanepeterson32@gmail.com>
2025-12-01 10:23:42 -0800

    feat: update revision for index.html in service worker for cache management

--- rowdy-ui/dev-dist/sw.js
+++ rowdy-ui/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-fa6cb374'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.q10c1rtks1o"
+    "revision": "0.9uncn0hi7s8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
