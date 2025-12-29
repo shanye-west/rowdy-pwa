6e4dc6149744f665d4269a23e34ded8a48f19dca
shanye-west <shanepeterson32@gmail.com>
2025-12-29 13:28:51 -0800

    Refactor code structure for improved readability and maintainability

--- rowdy-ui/dev-dist/sw.js
+++ rowdy-ui/dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-0aa01d1d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.2774ld0omho"
+    "revision": "0.af1r9iev20g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
